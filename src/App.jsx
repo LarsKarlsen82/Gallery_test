5dac38fd83c18ad1ffcfb4b25934f1e5a8676e40
LarsKarlsen82 <lpuk82@gmail.com>
2024-04-04 14:52:11 +0200

    Update

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
-
 import { useEffect, useState } from "react";
 import styled from "styled-components";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 import useContentful from "./useContentful";
 import AuthorCard from "./AuthorCard";
 
@@ -18,16 +20,43 @@ const App = () => {
     fetchData();
   }, [getAuthors]);
 
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
+  };
+
   return (
     <Wrapper>
       <TitleWrapper>
         <h1>Contentful API</h1>
-        <p>Galleri</p>
+        <br />
+        <h2>Galleri</h2>
       </TitleWrapper>
       <Gallery>
-        {authors.map((author, index) => (
-          <AuthorCard key={index} author={author} />
-        ))}
+        <Slider {...settings}>
+          {authors.map((author, index) => (
+            <AuthorCard key={index} author={author} />
+          ))}
+        </Slider>
       </Gallery>
     </Wrapper>
   );
@@ -47,7 +76,12 @@ const TitleWrapper = styled.div`
 `;
 
 const Gallery = styled.div`
-  display: grid;
-  grid-template-columns: repeat(auto-fill, minmax(240px, 1fr));
-  gap: 20px;
+  .slick-slide div {
+    display: flex;
+    justify-content: center;
+  }
+
+  .slick-dots {
+    bottom: 10px;
+  }
 `;
--- src/AuthorCard.jsx
+++ src/AuthorCard.jsx
@@ -1,49 +1,69 @@
+import { useState } from "react";
 import styled from "styled-components";
 
 const AuthorCard = ({ author }) => {
+  const [showDescription, setShowDescription] = useState(false);
+
+  const handleMouseEnter = () => {
+    // Show the title initially
+    setTimeout(() => {
+      setShowDescription(true);
+    }, 2000); // Show description after 3 seconds
+  };
+
+  const handleMouseLeave = () => {
+    setShowDescription(false);
+  };
+
   return (
-    <Wrapper background={author.avatar.file.url}>
+    <Wrapper
+      background={author.avatar.file.url}
+      onMouseEnter={handleMouseEnter}
+      onMouseLeave={handleMouseLeave}
+    >
       <TextContainer>
-        <Title>{author.name}</Title>
-        <Subtitle>{author.description}</Subtitle>
+        <Title isVisible={!showDescription}>{author.name}</Title>
+        <Subtitle isVisible={showDescription}>{author.description}</Subtitle>
       </TextContainer>
     </Wrapper>
   );
 };
 
-
 const Wrapper = styled.div`
-  display: grid;
-  align-items: flex-end;
-  width: 240px;
-  height: 240px;
+  position: relative;
+  width: 290px;
+  height: 290px;
   border-radius: 20px;
   box-shadow: 0px 20px 40px rgba(52, 53, 99, 0.2),
     0px 1px 3px rgba(0, 0, 0, 0.05);
   background: ${(props) =>
     props.background && `url(${props.background}) center no-repeat`};
   background-size: auto 150%;
+  transition: transform 0.3s ease-in-out;
+
   &:hover {
     transform: scale(1.05);
   }
 `;
 
 const TextContainer = styled.div`
-  cursor: pointer;
-  display: grid;
-  background: rgba(255, 255, 255, 0.3);
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 86.5%;
+  height: 100%;
   padding: 20px;
-  gap: 10px;
-  width: 83%;
+  background: rgba(255, 255, 255, 0.3);
+  border-radius: 20px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  opacity: 0;
+  transition: opacity 0.3s ease-in-out;
 
-  :hover {
-    height: fit-content;
-    width: auto;
-
-    p:last-child {
-      visibility: visible;
-      display: block;
-    }
+  ${Wrapper}:hover & {
+    opacity: 1;
   }
 `;
 
@@ -55,16 +75,19 @@ const Title = styled.p`
   font-weight: bold;
   color: #000000;
   margin: 0px;
+  visibility: ${(props) => (props.isVisible ? "visible" : "hidden")};
+  transition: visibility 0.3s ease-in-out;
 `;
 
 const Subtitle = styled.p`
   font-weight: normal;
-  font-size: 12px;
+  font-size: 18px;
   font-style: italic;
   color: rgba(0, 0, 0, 0.7);
   margin: 0px;
-  visibility: hidden;
-  display: none;
+  visibility: ${(props) => (props.isVisible ? "visible" : "hidden")};
+  display: ${(props) => (props.isVisible ? "block" : "none")};
+  transition: visibility 0.3s ease-in-out, display 0.3s ease-in-out;
 `;
 
 export default AuthorCard;
